ce2c81d363462f65556883201ade6038bb8d0a95
monishmadhu <monish9443370049@gmail.com>
2022-07-15 15:16:11 +0530

    hibro

---
+++ src/main/java/jenkinspro/App.java
@@ -0,0 +1,13 @@
+package jenkinspro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
